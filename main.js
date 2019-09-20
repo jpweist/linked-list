158899ae4fc0c1d2851ee4b4cd8e5da8f7d734e7
Jim Weist <jp_weist@yahoo.com>
2019-09-20 13:19:41 -0600

    The output user title and url are working need to refine css and it adds 2 cards for the first one on the second add of a card

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 // ### variables
 var pageEvents = document.querySelector('main');
 var enterButton = document.querySelector('button');
+var outputArticle = document.querySelector('.output-article');
 var eachNewBookMark = 0;
+var bookMarksArray = [];
 
 // ######### event listners
 pageEvents.addEventListener('click', function(event) {
-  // console.log(event.target.classList[0]);
+  console.log(event.target.classList[0]);
   // console.log(event.target.value);
 });
 
 enterButton.addEventListener('click', function() {
   getInputs();
+  // outputBookMarks();
+  outputBookMarks()
+
 
 });
-
 
 // ########## functions
 function getInputs() {
@@ -23,11 +27,33 @@ function getInputs() {
     console.log("the web site", userInputWeb.value);
     console.log("the URL", userInputURL.value);
     makeClass(userInputWeb.value, userInputURL.value)
+    checkInputs()
+
 
   }
+}
+function checkInputs() {
 }
 
 function makeClass(title, url) {
   bookmark1 = new Bookmark(title, url);
-  console.log(bookmark1);
+  bookMarksArray.push(bookmark1);
+  console.log(bookMarksArray, bookmark1);
+}
+
+function outputBookMarks() {
+  for (var i = 0; i < bookMarksArray.length; i++) {
+    var div = document.createElement('div');
+
+    div.innerHTML = `<input id='${bookMarksArray[i].id}' class='card'>
+    <h3>${bookMarksArray[i].title}</h3>
+    <hr/>
+    <p class="url">${bookMarksArray[i].url}</p>
+    <hr/>
+    <p class="read">Read</p>
+    <p class="delete">Delete</p></input> `;
+    outputArticle.appendChild(div);
+    // console.log(i);
+
+  }
 }
